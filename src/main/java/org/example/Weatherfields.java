b43ad8a57b372d4736d8d3c7f6677e93d2e9e0c7
Haidyg <130354388+Haidyg@users.noreply.github.com>
2024-07-14 01:08:43 +0300

    Add files via upload

---
+++ src/main/java/org/example/Weatherfields.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Weatherfields {
+   private double lat;
+   private double lon;
+   private double generationTimeMs;
+   private double utcOffsetSeconds;
+   private String timezone;
+   private String timezoneAbbreviation;
+   private float elevation;
+
+}
